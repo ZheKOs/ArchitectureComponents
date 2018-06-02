ca1d51baead503660857430511e8f23231faec97
Yevgen Yevdokimov <Yevgen.Yevdokimov@euvic.pl>
2018-06-03 00:38:34 +0200

    Lesson#3 MediatorLiveData

--- app/src/main/java/com/github/zhekos/architecturecomponents/MainActivity.kt
+++ app/src/main/java/com/github/zhekos/architecturecomponents/MainActivity.kt
@@ -1,28 +1,50 @@
 package com.github.zhekos.architecturecomponents
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.Transformations
+import android.arch.lifecycle.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val mediatorLiveData = MediatorLiveData<String>()
+
+    private val liveData1: MutableLiveData<String> = MutableLiveData()
+    private val liveData2: MutableLiveData<String> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val liveData: LiveData<String> = DataController.instance.getData()
+        mediatorLiveDataInit()
 
-        val liveDataInt: LiveData<Int> = Transformations.map(liveData, {
-            Integer.parseInt(it)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        liveData1.value = "1"
+        liveData2.value = "a"
+        liveData2.value = "finish"
+        liveData1.value = "2"
+        liveData2.value = "b"
+        liveData1.value = "3"
+        liveData2.value = "c"
+    }
+
+    private fun mediatorLiveDataInit(){
+        mediatorLiveData.addSource(liveData1, {
+            mediatorLiveData.value = it
+        })
+        mediatorLiveData.addSource(liveData2, {
+            if("finish".equals(it,true)){
+                mediatorLiveData.removeSource(liveData2)
+                return@addSource
+            }
+            mediatorLiveData.value = it
         })
 
-        liveData.observe(this, Observer<String> {
-            textview.text = it
+        mediatorLiveData.observe(this, Observer {
+            Log.d("onChanged", it)
         })
-        DataController.instance.setData("First String")
-
     }
 }
